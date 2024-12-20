d37026b83fa5f1c8ba8ac19e13c307b7a99f278d
nitinkumar-na <nitin.kumar@netapp.com>
2024-12-20 20:08:15 +0530

    feat: if else

--- projectTicketBook/main.go
+++ projectTicketBook/main.go
@@ -23,7 +23,7 @@ func main() {
 	var email string
 	var userTicket uint
 
-	for {
+	for remainingTickets > 0 && len(bookings) < 50 { // for { and for true { is same
 		// ask user for input
 		Println("Enter your first name:")
 		Scan(&firstName)
@@ -37,22 +37,30 @@ func main() {
 		Println("Enter number of tickets:")
 		Scan(&userTicket)
 
-		remainingTickets -= userTicket
-		bookings = append(bookings, firstName+" "+lastName)
+		if userTicket <= remainingTickets {
+			remainingTickets -= userTicket
+			bookings = append(bookings, firstName+" "+lastName)
 
-		Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTicket, email)
-		Printf("%v tikcets remaining for %v\n", remainingTickets, conferenceName)
+			Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTicket, email)
+			Printf("%v tikcets remaining for %v\n", remainingTickets, conferenceName)
 
-		// print first names
-		firstNames := []string{}
-		//var firstNames []string // -- alternate syntax
-		for _, fullName := range bookings { // _ here is the index
-			var names = strings.Fields(fullName)
-			fName := names[0]
-			firstNames = append(firstNames, fName)
+			// print first names
+			firstNames := []string{}
+			//var firstNames []string // -- alternate syntax
+			for _, fullName := range bookings { // _ here is the index
+				var names = strings.Fields(fullName)
+				fName := names[0]
+				firstNames = append(firstNames, fName)
+			}
+
+			Printf("These are all our bookings: %v\n", firstNames)
+			if remainingTickets == 0 {
+				Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			Printf("You can't book more than the available tickets i.e.: %v\n", remainingTickets)
 		}
-
-		Printf("These are all our bookings: %v\n", firstNames)
 	}
 
 }
