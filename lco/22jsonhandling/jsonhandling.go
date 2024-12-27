8d2cbe0f90e3312f5f02f3fbe60f2ef5fd0bd8cd
nitinkumar-na <nitin.kumar@netapp.com>
2024-12-27 12:55:43 +0530

    feat: json handling

--- lco/22jsonhandling/jsonhandling.go
+++ lco/22jsonhandling/jsonhandling.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("Welcome to JSON video")
-	encodeJson()
+	//encodeJson()
+	decodeJson()
 }
 
 func encodeJson() {
@@ -33,4 +34,35 @@ func encodeJson() {
 	}
 
 	fmt.Println(string(finalJson))
+}
+
+func decodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"courseName": "GoLang",
+		"Price": 299,
+		"website": "LCO.in",
+		"tags": ["web-dev","js"]
+	}`)
+
+	var lcoCourse course
+	checkValid := json.Valid(jsonDataFromWeb)
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON was invalid")
+	}
+
+	// other way of unMarshaling json in a map way
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+
+	fmt.Printf("Output2: %#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key: %v :: Value: %v :: Type: %T\n", k, v, v)
+	}
+
 }
