c4e58d55a442b4a119bb252fbe02326ad55753c7
nitinkumar-na <nitin.kumar@netapp.com>
2025-01-05 23:02:49 +0530

    feat: worker pool

--- projects/wokerpool/main.go
+++ projects/wokerpool/main.go
@@ -1,1 +1,25 @@
-package wokerpool
+package main
+
+import "fmt"
+
+func main() {
+	// using the worker pool
+
+	// create a new task
+	tasks := make([]Task, 20)
+	for i := 0; i < 20; i++ {
+		tasks[i] = Task{ID: i + 1}
+	}
+
+	// create a worker pol with the task and set the concurrency limit
+
+	wp := WorkerPool{
+		Tasks:            tasks,
+		concurrencyLimit: 5, // no. of workers that can run at a time
+	}
+
+	// start the worker pool to process the tasks
+	wp.Run()
+
+	fmt.Println("All the tasks has been processed")
+}
--- projects/wokerpool/workerpool.go
+++ projects/wokerpool/workerpool.go
@@ -1,3 +1,65 @@
-package wokerpool
+package main
 
-// Single unit of work
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Single unit of work - Task Definition
+type Task struct {
+	ID int
+}
+
+// function to process the Task - Way to process the task
+func (t *Task) Process() {
+	fmt.Printf("Processing task %d\n", t.ID)
+	// Simulate a time consuming process
+	time.Sleep(2 * time.Second)
+}
+
+// Worker Pool Definition
+type WorkerPool struct {
+	Tasks            []Task         // Tasks List
+	concurrencyLimit int            // No of concurrent process that can be handled together
+	tasksChan        chan Task      // defines a channel to send task to workers
+	wg               sync.WaitGroup // Synchronising the completion of task. This will we used to wait for the task to be completed
+}
+
+// Worker Pool function, to execute the task in the pool
+
+// method that receives tasks from the task channels and processes them
+// defining a method on WorkerPool Struct
+
+func (pool *WorkerPool) worker() {
+	for task := range pool.tasksChan {
+		task.Process()
+		pool.wg.Done()
+	}
+}
+
+// The run method: This method initialises the channel, sets the concurrency, creates the go routines ans sends tasks
+// over the channel
+func (pool *WorkerPool) Run() {
+	// Initialize the tasks channel with the capacity equals the number of tasks
+	pool.tasksChan = make(chan Task, len(pool.Tasks))
+
+	// Start the worker go routines, number of workers equals to the concurrency variable
+	for i := 0; i < pool.concurrencyLimit; i++ {
+		go pool.worker()
+	}
+
+	// add the number of tasks in the wait group counter
+	pool.wg.Add(len(pool.Tasks))
+
+	// send tasks to the task channel
+	for _, task := range pool.Tasks {
+		pool.tasksChan <- task
+	}
+
+	// close the task chan after sending all tasks to Signal no more tasks will be sent
+	close(pool.tasksChan)
+
+	// wait for all the tasks to be processed using the wait method of the wait group
+	pool.wg.Wait()
+}
