41df7f34a416ca5196a2601f390122b68bba7947
nitinkumar-na <nitin.kumar@netapp.com>
2025-01-19 13:16:02 +0530

    feat: context with timeout

--- lco/30contextPkg/main.go
+++ lco/30contextPkg/main.go
@@ -16,20 +16,42 @@ func main() {
 	val, err := callApi(ctx, userId)
 
 	if err != nil {
-		log.Fatal("Error")
+		log.Fatalf("Error: %v", err)
 	}
 
 	fmt.Println("Result:", val)
 	fmt.Println("Total Time Take:", time.Since(start))
 }
 
-func callApi(ctx context.Context, userId int) (int, error) {
-	val, err := mimicAPICallWithDelay(2, time.Second)
-	if err != nil {
-		return 0, err
-	}
+type Response struct {
+	value int
+	err   error
+}
 
-	return val, nil
+func callApi(ctx context.Context, userId int) (int, error) {
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*200)
+	defer cancel()
+
+	respChn := make(chan Response)
+
+	go func() {
+		//val, err := mimicAPICallWithDelay(2, time.Nanosecond)
+		val, err := mimicAPICallWithDelay(2, time.Second)
+		respChn <- Response{
+			value: val,
+			err:   err,
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return 0, fmt.Errorf("request timed out")
+
+		case resp := <-respChn:
+			return resp.value, resp.err
+		}
+	}
 }
 
 func mimicAPICallWithDelay(delayValue int64, duration time.Duration) (int, error) {
